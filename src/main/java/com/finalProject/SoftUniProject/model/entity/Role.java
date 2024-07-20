fc6c25f5b86d6214194317cc4450b02f1bec30c3
alexdkv <alexdekov120@gmail.com>
2024-07-20 21:16:07 +0300

    Roles in front-end.

--- src/main/java/com/finalProject/SoftUniProject/model/entity/Role.java
+++ src/main/java/com/finalProject/SoftUniProject/model/entity/Role.java
@@ -25,7 +25,7 @@ public class Role {
         this.id = id;
     }
 
-    public @NotNull UserRoleENUM getName() {
+    public UserRoleENUM getName() {
         return name;
     }
 
--- src/main/java/com/finalProject/SoftUniProject/service/UserDetailsService.java
+++ src/main/java/com/finalProject/SoftUniProject/service/UserDetailsService.java
@@ -1,9 +1,15 @@
 package com.finalProject.SoftUniProject.service;
 
+import com.finalProject.SoftUniProject.model.entity.Role;
 import com.finalProject.SoftUniProject.model.entity.User;
+import com.finalProject.SoftUniProject.model.enums.UserRoleENUM;
 import com.finalProject.SoftUniProject.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
 
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
@@ -24,11 +30,30 @@ public class UserDetailsService implements org.springframework.security.core.use
     }
 
     private static UserDetails map(User user){
+
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().getName());
+
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getEmail())
                 .password(user.getPassword())
-                //.authorities()
+                .authorities(authority)
                 .disabled(false)
                 .build();
+
+        /*
+        return new com.finalProject.SoftUniProject.model.UserDetails(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole().stream().map(r -> r.).map(UserDetailsService::map).toList());
+
+         */
     }
+/*
+    private static GrantedAuthority map(UserRoleENUM role) {
+        return new SimpleGrantedAuthority(
+                "ROLE_" + role
+        );
+    }
+
+ */
 }
