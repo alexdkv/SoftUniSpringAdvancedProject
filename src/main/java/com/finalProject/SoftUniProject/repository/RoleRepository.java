2a94bffd230bdeb2bc59381a8a6972b631631d95
alexdkv <alexdekov120@gmail.com>
2024-07-22 15:03:01 +0300

    Add Exercise page

--- src/main/java/com/finalProject/SoftUniProject/repository/RoleRepository.java
+++ src/main/java/com/finalProject/SoftUniProject/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.finalProject.SoftUniProject.repository;
 import com.finalProject.SoftUniProject.model.entity.Role;
 import com.finalProject.SoftUniProject.model.enums.UserRoleENUM;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(UserRoleENUM name);
 }
--- src/main/java/com/finalProject/SoftUniProject/repository/UserRepository.java
+++ src/main/java/com/finalProject/SoftUniProject/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.finalProject.SoftUniProject.repository;
 
 import com.finalProject.SoftUniProject.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     Optional<User> findByEmail(String email);
