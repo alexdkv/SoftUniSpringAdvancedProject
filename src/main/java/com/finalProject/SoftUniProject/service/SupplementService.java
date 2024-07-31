f02670d1a09b14dfa87ddc558efe5a56e61b25cb
alexdkv <alexdekov120@gmail.com>
2024-07-31 17:47:03 +0300

    Rest Client add

--- src/main/java/com/finalProject/SoftUniProject/service/SupplementService.java
+++ src/main/java/com/finalProject/SoftUniProject/service/SupplementService.java
@@ -1,7 +1,23 @@
 package com.finalProject.SoftUniProject.service;
 
+import com.finalProject.SoftUniProject.model.dto.SupplementAddDTO;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class SupplementService {
+
+    private final RestClient supplementRestClient;
+
+    public SupplementService(@Qualifier("supplementsRestClient") RestClient supplementRestClient) {
+        this.supplementRestClient = supplementRestClient;
+    }
+
+    public void createSupplement(SupplementAddDTO supplementAddDTO){
+        supplementRestClient.post()
+                .uri("http://localhost:8081/supplement")
+                .body(supplementAddDTO)
+                .retrieve();
+    }
 }
--- src/main/java/com/finalProject/SoftUniProject/web/SupplementController.java
+++ src/main/java/com/finalProject/SoftUniProject/web/SupplementController.java
@@ -1,16 +1,29 @@
 package com.finalProject.SoftUniProject.web;
 
 import com.finalProject.SoftUniProject.model.dto.SupplementAddDTO;
+import com.finalProject.SoftUniProject.service.SupplementService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SupplementController {
+    private final SupplementService supplementService;
+
+    public SupplementController(SupplementService supplementService) {
+        this.supplementService = supplementService;
+    }
 
     @GetMapping("/supplement-add")
     public ModelAndView supplementAddView(@ModelAttribute("supplementAddDTO") SupplementAddDTO supplementAddDTO){
         return new ModelAndView("supplement-add");
+    }
+
+    @PostMapping("/supplement-add")
+    public ModelAndView supplementAdd(@ModelAttribute("supplementAddDTO") SupplementAddDTO supplementAddDTO){
+        supplementService.createSupplement(supplementAddDTO);
+        return new ModelAndView("redirect:/home");
     }
 }
